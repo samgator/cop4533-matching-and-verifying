396572a336ee39426256374cc02f24b46d1e7e6e
Jayden <jtbow1111@gmail.com>
2026-01-29 16:05:47 -0500

    Gale Shapely added + main function with print + added my student number
    Implemented Gale Shapely function, pretty sure its working. Also made a main function with print statement and added my student number to the readme

--- src/matcher.py
+++ src/matcher.py
@@ -15,11 +15,59 @@ def get_input(filename):
 
         return n, hospital_preferences, student_preferences
 
-# Get filepath from command line arguments and read input
-input_filepath = sys.argv[1]
-n, hospital_prefs, student_prefs = get_input(input_filepath)
+#function for gale_shapely algorithm based on the lecture slide psuedocode
+def gale_shapely(n, hospital_preferences, student_preferences):
+    # Initialize and track student index
+    hospital_match = [-1]*n
+    student_match = [-1]*n
+    proposals_sent = [0]*n
 
-# Prints for debugging (to be removed)
-print("n =", n)
-print("Hospital preferences:", hospital_prefs)
-print("Student preferences:", student_prefs)
+    #some hospital is free and hasn't been matched/assigned to every applicant
+    while -1 in hospital_match:
+        h = hospital_match.index(-1)
+        # The hospital has tried proposing to everyone so break
+        if proposals_sent[h] >= n:
+            break
+
+        #1st applicant (a) on h's list to whom h has not been matched
+        ideal_student = hospital_preferences[h][proposals_sent[h]]-1
+        proposals_sent[h] += 1
+
+        #if a is free assign h and a 
+        if student_match[ideal_student] == -1:
+            student_match[ideal_student] = h
+            hospital_match[h] = ideal_student
+        else:
+            h_prime = student_match[ideal_student]
+            s_pref_list = student_preferences[ideal_student]
+            #a prefers h to their current h'
+            if s_pref_list.index(h+1) < s_pref_list.index(h_prime + 1):
+                student_match[ideal_student] = h
+                hospital_match[h] = ideal_student
+                hospital_match[h_prime] = -1
+            #a rejects h
+            else:
+                hospital_match[h] = -1
+    return hospital_match
+
+def main():
+    # Get filepath from command line arguments and read input
+    input_filepath = sys.argv[1]
+    n, hospital_prefs, student_prefs = get_input(input_filepath)
+
+    # Prints for debugging (to be removed)
+    print("n =", n)
+    print("Hospital preferences:", hospital_prefs)
+    print("Student preferences:", student_prefs)
+
+    if n == 0:
+        return
+    
+    match_final = gale_shapely(n, hospital_prefs, student_prefs)
+
+    for i in range(n):
+        if match_final[i] != -1:
+            print(i + 1, match_final[i] + 1)
+
+if __name__ == "__main__":
+    main()
